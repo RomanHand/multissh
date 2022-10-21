bd20902938cb666927e946b434cf299b43331dc7
RomanHand <roma25r@gmail.com>
2022-10-21 17:46:46 +1000

    add base func

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import paramiko
+import time
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(
+    paramiko.AutoAddPolicy())
+ssh.connect('10.240.230.121', username='monitor', password='ess')
+command = 'ls'
+(stdin, stdout, stderr) = ssh.exec_command(command)
+#stdin.write('ess\n')
+time.sleep(1.0)
+for line in stdout.readlines():
+        print(line, end = '')
+ssh.close()
